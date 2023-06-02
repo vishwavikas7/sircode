ba9bccd1455bf4e112018af412bccee76026b9ec
kesav.kummari@gmail.com-at-420815905200 <devops.kesav@gmail.com>
2023-06-02 12:21:56 +0530

    iac code has been added

---
+++ iac/var.tf
@@ -0,0 +1,16 @@
+
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  default = "default"
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
